3eed9b53b1988a645060d0cce67940410e463cb6
Tim Hoolihan <tim@hoolihan.net>
2014-10-24 12:21:38 -0400

    Refactor and fix some math errors

--- Picks.R
+++ Picks.R
@@ -2,61 +2,69 @@ library("scales")
 
 nfl = read.csv("NFL_Picks.csv")
 results = nfl[complete.cases(nfl),]
-n_results = nrow(results)
 
-n_correct = sum(results$Correct)
-correct_picks_pct = percent(n_correct / n_results)
+any_pick <- function(g) { g$Year == g$Year }
+correct_pick <- function(g) { g$Correct }
+home_fav <- function(g) { g$Favorite == 'H' }
+away_fav <- function(g) { g$Favorite == 'A' }
+home_fav_covers <- function(g) { g$Home_Score > (g$Away_Score + g$Spread) & home_fav(g) }
+away_fav_covers <- function(g) { g$Away_Score > (g$Home_Score + g$Spread) & away_fav(g) }
+fav_covers <- function(g) { home_covers(g) | away_covers(g) }
+pick_home <- function(g) { as.character(g$Pick) == as.character(g$Home_Team) }
+pick_away <- function(g) { as.character(g$Pick) == as.character(g$Away_Team) }
+pick_favorite <- function (g) { (home_fav(g) & pick_home(g)) | (away_fav(g) & pick_away(g)) }
+pick_underdog <- function (g) { !pick_favorite(g) }
+pick_home_favorite <- function(g) { pick_home(g) & pick_favorite(g) }
+pick_away_favorite <- function(g) { pick_away(g) & pick_favorite(g) }
+pick_home_dog <- function(g) { pick_home(g) & pick_underdog(g) }
+pick_away_dog <- function(g) { pick_away(g) & pick_underdog(g) }
 
-v_away_covers = results$Away_Score > (results$Home_Score + results$Spread) & results$Favorite == 'A'
-v_home_covers = results$Home_Score > (results$Away_Score + results$Spread) & results$Favorite == 'H'
-n_home_fav_covers = sum(v_home_covers)
-n_away_fav_covers = sum(v_away_covers)
+condition_frequency <- function(data, subset_condition = any_pick) {
+  sum(subset_condition(data))
+}
 
-#your home picks
-v_pick_home = (as.character(results$Pick) == as.character(results$Home_Team))
-v_pick_home_correct = v_pick_home & v_home_covers
-n_pick_home = sum(v_pick_home)
-pick_home_pct = percent(n_pick_home / n_results)
-pick_home_correct_pct = percent(sum(v_pick_home_correct) / n_pick_home)
+condition_percentage <- function(data, 
+                                 set_condition = any_pick, 
+                                 subset_condition = correct_pick) {
+  subset_filter <- function(g) { set_condition(g) & subset_condition(g) }
+  condition_frequency(data, subset_filter) / condition_frequency(data, set_condition)
+}
 
-#your away picks
-v_pick_away = (as.character(results$Pick) == as.character(results$Away_Team))
-v_pick_away_correct = v_pick_away & v_away_covers
-n_pick_away = sum(v_pick_away)
-pick_away_pct = percent(n_pick_away / n_results)
-pick_away_correct_pct = percent(sum(v_pick_away_correct) / n_pick_away)
+#league trends
+games <- condition_frequency(results)
+weeks <- length(unique(results$Week))
 
-#your favorite picks
-v_pick_home_fav = (results$Favorite == 'H') & v_pick_home
-v_pick_away_fav = (results$Favorite == 'A') & v_pick_away
-v_pick_fav = v_pick_home_fav | v_pick_away_fav
-v_pick_fav_correct = v_pick_fav & (v_home_covers | v_away_covers)
-n_pick_fav = sum(v_pick_fav)
-pick_fav_pct = percent(n_pick_fav / n_results)
-pick_fav_correct_pct = percent(sum(v_pick_fav_correct) / n_pick_fav)
+home_fav_pct <- condition_percentage(results, subset_condition = home_fav)
+home_fav_covers_pct <- condition_percentage(results, set_condition = home_fav, subset_condition = home_covers)
 
-#your home favorite picks
-n_pick_home_fav = sum(v_pick_home_fav)
-pick_home_fav_pct = percent(n_pick_home_fav / n_results)
-pick_home_fav_correct_pct = percent(sum(v_pick_home_fav & v_home_covers) / n_pick_home_fav)
+away_fav_pct <- condition_percentage(results, subset_condition = away_fav)
+away_fav_covers_pct <- condition_percentage(results, set_condition = away_fav, subset_condition = away_covers)
 
-#your away favorite picks
-n_pick_away_fav = sum(v_pick_away_fav)
-pick_away_fav_pct = percent(n_pick_away_fav / n_results)
-pick_away_fav_correct_pct = percent(sum(v_pick_away_fav & v_away_covers) / n_pick_away_fav)
+fav_covers_pct <- condition_percentage(results, subset_condition = fav_covers)
 
-#your underdog picks
-v_pick_home_dog = (results$Favorite == 'A') & (as.character(results$Pick) == as.character(results$Home_Team))
-v_pick_away_dog = (results$Favorite == 'H') & (as.character(results$Pick) == as.character(results$Away_Team))
-v_pick_dog = v_pick_home_dog | v_pick_away_dog
-v_pick_dog_correct = v_pick_dog & !(v_home_covers | v_away_covers)
-n_pick_dog = sum(v_pick_dog)
-pick_dog_pct = percent(n_pick_dog / n_results)
-pick_dog_correct_pct = percent(sum(v_pick_dog_correct) / n_pick_dog)
+#your trends
+correct_picks_pct <- condition_percentage(results)
 
-# how many home teams that are favored cover
-home_fav_covers = percent(n_home_fav_covers / sum(results$Favorite == 'H'))
-# how many away teams that are favored cover
-away_fav_covers = percent(n_away_fav_covers / sum(results$Favorite == 'A'))
-#how many favored teams cover
-fav_covers = percent((n_home_fav_covers + n_away_fav_covers) / n_results)
+pick_home_pct <- condition_percentage(results, subset_condition = pick_home)
+pick_home_correct_pct <- condition_percentage(results, set_condition = pick_home)
+
+pick_away_pct <- condition_percentage(results, subset_condition = pick_away)
+pick_away_correct_pct <- condition_percentage(results, set_condition = pick_away)
+
+pick_fav_pct <- condition_percentage(results, subset_condition = pick_favorite)
+pick_fav_correct_pct <- condition_percentage(results, set_condition = pick_favorite)
+
+pick_dog_pct <- condition_percentage(results, subset_condition = pick_underdog)
+pick_dog_correct_pct <- condition_percentage(results, set_condition = pick_underdog)
+
+pick_home_fav_pct <- condition_percentage(results, subset_condition = pick_home_favorite)
+pick_home_fav_correct_pct <- condition_percentage(results, set_condition = pick_home_favorite)
+
+pick_away_fav_pct <- condition_percentage(results, subset_condition = pick_away_favorite)
+pick_away_fav_correct_pct <- condition_percentage(results, set_condition = pick_away_favorite)
+
+pick_home_dog_pct <- condition_percentage(results, subset_condition = pick_home_dog)
+pick_home_dog_correct_pct <- condition_percentage(results, set_condition = pick_home_dog)
+
+pick_away_dog_pct <- condition_percentage(results, subset_condition = pick_away_dog)
+pick_away_dog_correct_pct <- condition_percentage(results, set_condition = pick_away_dog)
