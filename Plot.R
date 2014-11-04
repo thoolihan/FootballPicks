d42d1a85fdfdf53c914462f1d57d5f166b4fc1f6
Tim Hoolihan <tim@hoolihan.net>
2014-11-04 15:47:28 -0500

    Moving results plot to ggplot2

--- Plot.R
+++ Plot.R
@@ -4,14 +4,14 @@ results_2014 = results[results$Year == 2014,]
 games_by_week = aggregate(results_2014$Pick, by=list(Week = results$Week), FUN=length)
 half <- games_by_week
 half$x <- half$x * 0.5
-res_by_wk = aggregate(results_2014$Correct, by=list(Week = results$Week), FUN=sum)
-frame()
-par(new=T, mfrow=c(1,1))
-plot(res_by_wk, axes=F, type="b", xlim=c(1,21), ylim=c(0,16),
-     main="Tim's 2014 NFL Pick Spread Results By Week", xlab="Week", ylab="Correct", col="Green")
-lines(games_by_week, col="Blue", type="b")
-lines(half, col="Gray", type="l")
-axis(side=1, at=c(1:21))
-axis(side=2, at=c(0:16))
-legend(x=18, y=16, legend=c('Games', 'Correct', '50%'), fill=c('Blue', 'Green', 'Gray'))
-box()
+res_by_week = aggregate(results_2014$Correct, by=list(Week = results$Week), FUN=sum)
+
+res <- data.frame(Week = games_by_week$Week,
+                  Total = games_by_week$x,
+                  Correct = res_by_week$x,
+                  Half = half$x)
+
+print(ggplot(res, aes(Week), main="Foo") +
+        geom_line(aes(y = Total, color="Total")) +
+        geom_point(aes(y = Correct, color="Correct")) +
+        geom_line(aes(y = Half, color="50%")))
