75294c9eb79478782f12c0859dd21e1f2beabe1b
Tim Hoolihan <tim@hoolihan.net>
2014-11-04 13:11:14 -0500

    Updated label

--- Plot.R
+++ Plot.R
@@ -8,7 +8,7 @@ res_by_wk = aggregate(results_2014$Correct, by=list(Week = results$Week), FUN=su
 frame()
 par(new=T, mfrow=c(1,1))
 plot(res_by_wk, axes=F, type="b", xlim=c(1,21), ylim=c(0,16),
-     main="Tim's 2014 NFL Pick results By Week", xlab="Week", ylab="Correct", col="Green")
+     main="Tim's 2014 NFL Pick Spread Results By Week", xlab="Week", ylab="Correct", col="Green")
 lines(games_by_week, col="Blue", type="b")
 lines(half, col="Gray", type="l")
 axis(side=1, at=c(1:21))
