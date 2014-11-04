b721ffc55f19fe9833b23c59c615915a1cb74783
Tim Hoolihan <tim@hoolihan.net>
2014-11-04 15:52:32 -0500

    Simplified plot call, moved calculations inline

--- Plot.R
+++ Plot.R
@@ -1,17 +1,11 @@
 source("./Picks.R")
+library("ggplot2")
 
 results_2014 = results[results$Year == 2014,]
 games_by_week = aggregate(results_2014$Pick, by=list(Week = results$Week), FUN=length)
-half <- games_by_week
-half$x <- half$x * 0.5
 res_by_week = aggregate(results_2014$Correct, by=list(Week = results$Week), FUN=sum)
 
-res <- data.frame(Week = games_by_week$Week,
-                  Total = games_by_week$x,
-                  Correct = res_by_week$x,
-                  Half = half$x)
-
-print(ggplot(res, aes(Week), main="Foo") +
-        geom_line(aes(y = Total, color="Total")) +
-        geom_point(aes(y = Correct, color="Correct")) +
-        geom_line(aes(y = Half, color="50%")))
+print(ggplot(res_by_week, aes(Week)) +
+        geom_line(aes(y = games_by_week$x, color="Total")) +
+        geom_point(aes(y = res_by_week$x, color="Correct")) +
+        geom_line(aes(y = games_by_week$x * 0.5, color="50%")))
