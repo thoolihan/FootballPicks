18cabc856efde294de9e093ac9ab4c541b32cf2f
Tim Hoolihan <tim@hoolihan.net>
2014-11-04 13:27:30 -0500

    Whitespace fixes

--- PlotTeams.R
+++ PlotTeams.R
@@ -5,7 +5,7 @@ covers <- team_data
 covers$FavCoverPct <- ifelse(is.na(covers$FavCoverPct), 0.0, covers$FavCoverPct)
 covers <- covers[order(covers$FavCoverPct),]
 with(covers, {
-  dotchart(FavCoverPct * 100, labels = Team, 
+  dotchart(FavCoverPct * 100, labels = Team,
            main="% Covering The Spread", xlab="Cover %",
            xaxt="n", col="red")
 })
@@ -14,7 +14,7 @@ upsets <- team_data
 upsets$UpsetPct <- ifelse(is.na(upsets$UpsetPct), 0.0, upsets$UpsetPct)
 upsets <- upsets[order(upsets$UpsetPct),]
 with(upsets, {
-  dotchart(UpsetPct * 100, labels = Team, 
+  dotchart(UpsetPct * 100, labels = Team,
            main="% Upset When UnderDog", xlab="Upset %",
            xaxt="n", col="blue")
 })
