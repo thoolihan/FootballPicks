914c2bdc3c5ca6dd11ffdee79c2da01e26462242
Tim Hoolihan <tim@hoolihan.net>
2014-11-04 17:36:34 -0500

    Improve plotting

--- Plot.R
+++ Plot.R
@@ -5,7 +5,15 @@ results_2014 = results[results$Year == 2014,]
 games_by_week = aggregate(results_2014$Pick, by=list(Week = results$Week), FUN=length)
 res_by_week = aggregate(results_2014$Correct, by=list(Week = results$Week), FUN=sum)
 
-print(ggplot(res_by_week, aes(Week)) +
-        geom_line(aes(y = games_by_week$x, color="Total")) +
-        geom_point(aes(y = res_by_week$x, color="Correct", size=10)) +
-        geom_line(aes(y = games_by_week$x * 0.5, color="50%")))
+projected = coef(lm(res_by_week$x ~ res_by_week$Week))
+
+print(ggplot(res_by_week, aes(Week, x)) +
+        stat_smooth(aes(color = "Projected"), method="lm", color="purple", linetype="dotted") +
+        geom_line(aes(y = games_by_week$x, color="Total"), color = "black", linetype = "dashed") +
+        geom_point(aes(y = res_by_week$x, color="Correct", size = 10), color = "dark green") +
+        geom_line(aes(y = games_by_week$x * 0.5, color="50%"),
+                  linetype = 'dashed', color = "blue") +
+        xlim(1, 17) +
+        ylim(0, 16) +
+        ylab('Picks'))
+
