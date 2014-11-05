08aed5de687b883eb8214e1ceb46f146337545f3
Tim Hoolihan <tim@hoolihan.net>
2014-11-04 22:30:34 -0500

    Use dplyr for cleaner data cleaning

--- PlotTeams.R
+++ PlotTeams.R
@@ -1,19 +1,23 @@
 source("Teams.R")
 library("ggplot2")
+library("dplyr")
 
-covers <- team_data[order(team_data$FavCoverPct, team_data$UpsetPct),]
-covers$Team <- factor(covers$Team, levels = covers$Team, ordered=TRUE)
+covers <- mutate(team_data,
+                 FavCoverPct = ifelse(is.na(FavCoverPct), 0.0, FavCoverPct),
+                 UpsetPct = ifelse(is.na(UpsetPct), 0.0, UpsetPct)
+)
 
-covers$FavCoverPct <- ifelse(is.na(covers$FavCoverPct), 0.0, covers$FavCoverPct)
-covers$UpsetPct <- ifelse(is.na(covers$UpsetPct), 0.0, covers$UpsetPct)
+covers <- arrange(team_data, FavCoverPct, UpsetPct)
+covers <- mutate(covers, Team = factor(Team, levels = Team, ordered=TRUE))
 
 visuals <- ggplot(data = covers) +
   geom_point(aes(y = Team,
                  x = FavCoverPct * 100,
                  color="Cover %",
-                 size = Favorited)) +
+                 size = Favorited), color = "Blue") +
   geom_point(aes(y = Team,
                  x = UpsetPct * 100,
                  color = "Upset %",
-                 size = Underdog))
+                 size = Underdog), color = "Red") +
+  xlab("% Cover or Upset")
 print(visuals)
