6b8ffe60a5728570489dc4691b99bb288b5e852a
Tim Hoolihan <tim@hoolihan.net>
2014-11-25 06:12:54 -0500

    Plotting a normal distribution around the home score

--- historical.R
+++ historical.R
@@ -1,3 +1,4 @@
+library(ggplot2)
 # analyze data from http://www.repole.com/sun4cast/data.html
 nfl_dir <- '~/workspace/data/nfl/'
 
@@ -16,3 +17,11 @@ print(sprintf("Home Average: %0.8f", mean(all_nfl$Home.Score)))
 print(sprintf("Away Average: %0.8f", mean(all_nfl$Visitor.Score)))
 print(sprintf("Margin of Victory: %0.8f",
               mean(all_nfl$Home.Score) - mean(all_nfl$Visitor.Score)))
+
+hsv <- rnorm(5000, mean = 22.3, sd = 7)
+hsd <- dnorm(hsv, mean = 22.3, sd = 7)
+p <- ggplot(data.frame(x = hsv, y = hsd)) +
+  aes(x = x, y = y) +
+  geom_point(color = "blue") +
+  labs(x = "Home Score", y = "Probability")
+print(p)
